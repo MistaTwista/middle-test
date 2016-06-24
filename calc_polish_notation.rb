92aeda100345b132eea6608ee4389bd245aed328
maxim <ctajikep@gmail.com>
2016-06-24 15:32:24 +0300

    Polish notation calculator without ZeroDivisionError handling

--- calc_polish_notation.rb
+++ calc_polish_notation.rb
@@ -1,17 +1,30 @@
-def calculation_sign?(symbol)
-  %w(+ - * / ^).include? symbol
+def calculation_sign(symbol)
+  available_sign_symbols = {
+    "+": :+,
+    "-": :-,
+    "*": :*,
+    "/": :/,
+    "^": :**,
+  }
+  available_sign_symbols[symbol.to_sym]
 end
 
-# Отрицательные числа
-# ^ ??
-# Если передано пустое выражение = 0
-# 3 4 2 * 1 5 − 2 3 ^ ^ / +
+def valid_expression?(exp)
+  return false unless /[^ \+\-\*\/\^0-9]/.match(exp.strip).nil?
+  if exp.is_a?(String)
+    return true unless exp.strip.empty?
+  end
+  false
+end
+
 def calc_polish_notation(exp)
-  expression = exp.split(" ")
+  return 0 unless valid_expression? exp
+  expression = exp.strip.split(" ")
   [].tap do |stack|
     expression.each do |u|
-      if calculation_sign? u
-        stack << stack.pop(2).map(&:to_i).reduce(&u.to_sym)
+      sign = calculation_sign(u)
+      if sign
+        stack << stack.pop(2).map(&:to_i).reduce(&sign)
       else
         stack << u
       end
--- test_calc_polish_notation.rb
+++ test_calc_polish_notation.rb
@@ -3,10 +3,26 @@ require_relative 'calc_polish_notation'
 
 class TestCalcPolishNotation < Test::Unit::TestCase
   def test_calculation_sign
-    assert_equal(true, calculation_sign?("+"))
+    assert_equal(:+, calculation_sign("+"))
+    assert_equal(:-, calculation_sign("-"))
+    assert_equal(:*, calculation_sign("*"))
+    assert_equal(:/, calculation_sign("/"))
+    assert_equal(:**, calculation_sign("^"))
+  end
+
+  def test_valid_expression
+    assert_equal(true, valid_expression?("5 1 2 + 4 * + 3 -"))
+    assert_equal(true, valid_expression?("  5 1 2 + 4 * + 3 -  "))
+    assert_equal(false, valid_expression?("3 4 2 * 1 5 − 2 3 ^ ^ / +"))
+    assert_equal(false, valid_expression?("  "))
+    assert_equal(false, valid_expression?(""))
   end
 
   def test_calc_polish_notation
     assert_equal(14.0, calc_polish_notation("5 1 2 + 4 * + 3 -"))
+    assert_equal(3, calc_polish_notation("3 4 2 * 1 5 - 2 3 ^ ^ / +"))
+    assert_equal(25, calc_polish_notation("5 2 ^"))
+    assert_equal(0, calc_polish_notation(" "))
+    assert_equal(0, calc_polish_notation(""))
   end
 end
